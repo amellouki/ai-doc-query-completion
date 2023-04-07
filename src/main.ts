dae0c3b70aa2a3410e00a472f80d4b61cfef501c
mellouki <abderrahim.mellouki.p@gmail.com>
2023-04-07 18:29:58 +0000

    export some of cors config to env

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow only this specific origin
+    origin: [process.env.ALLOWED_LOCALHOST, process.env.ALLOWED_DOMAIN], // Allow these origins
     methods: 'GET,HEAD', // Allow these HTTP methods
     allowedHeaders: 'Content-Type, Accept', // Allow these headers
     credentials: true, // Allow cookies and credentials to be sent with requests
